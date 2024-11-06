b48d23d94fcd5f6a752ae3854d6143506d06ce9c
shalini322 <155485757+shalini322@users.noreply.github.com>
2024-11-06 22:51:36 +0530

    questionBank.js

---
+++ components/questionBank.js
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "Mark Twain"],
+        answer: "William Shakespeare",
+    },
+    {
+        id: 2,
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Venus", "Mars", "Jupiter"],
+        answer: "Mars",
+    },
+    {
+        id: 3,
+        question: "What is the boiling point of water?",
+        options: ["50°C", "100°C", "150°C", "200°C"],
+        answer: "100°C",
+    },
+    {
+        id: 4,
+        question: "Who is known as the father of computers?",
+        options: ["Albert Einstein", "Charles Babbage", "Isaac Newton", "Nikola Tesla"],
+        answer: "Charles Babbage",
+    },
+    {
+        id: 5,
+        question: "What is the largest ocean on Earth?",
+        options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        answer: "Pacific Ocean",
+    },
+];
+
+export default qBank;
